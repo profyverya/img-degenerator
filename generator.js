301b43922c497cab91b5637716db63687dff45f1
profyverya <39007853+profyverya@users.noreply.github.com>
2020-06-01 10:43:34 +0300

    Update generator.js

--- generator.js
+++ generator.js
@@ -24,25 +24,22 @@ function generate(){
     im.src = fr.result;
   }
 }
-function wrapText(context, text, marginLeft, marginTop, maxWidth, lineHeight)
-    {
-        var words = text.split(" ");
-        var countWords = words.length;
-        var line = "";
-        for (var n = 0; n < countWords; n++) {
-            var testLine = line + words[n] + " ";
-            var testWidth = context.measureText(testLine).width;
-            if (testWidth > maxWidth) {
-                context.fillText(line, marginLeft, marginTop);
-                line = words[n] + " ";
-                marginTop += lineHeight;
-            }
-            else {
-                line = testLine;
-            }
-        }
-        context.fillText(line, marginLeft, marginTop);
+function wrapText(context, text, marginLeft, marginTop, maxWidth, lineHeight){
+  var words = text.split(" ");
+  var line = "";
+  for (var n = 0; n < words.length; n++) {
+    var testLine = line + words[n] + " ";
+    var testWidth = context.measureText(testLine).width;
+    if (testWidth > maxWidth) {
+      context.fillText(line, marginLeft, marginTop);
+      line = words[n] + " ";
+      marginTop += lineHeight;
+    } else {
+      line = testLine;
     }
+  }
+  context.fillText(line, marginLeft, marginTop);
+}
 function download(canvas) {
   document.getElementById("imag").src = canvas.toDataURL("image/png;base64");
 }
